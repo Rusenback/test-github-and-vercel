85ef77cfd3cb527e3eefffca54f1cf683f47aad6
Petteri Rainoma <petterirainoma@kamk.fi>
2025-04-07 15:36:20 +0300

    muutettu numero

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("<H1 style=color:blue;>Express on Vercel is working! 2</H1>"));
+app.get("/", (req, res) => res.send("<H1 style=color:blue;>Express on Vercel is working! 4</H1>"));
 app.listen(4000, () => console.log("Server ready on port 4000."));
 
 module.exports = app;
