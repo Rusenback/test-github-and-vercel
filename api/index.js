2145e9f20fb0d9f6b6d12862c0aad45376c63b0e
Petteri Rainoma <petterirainoma@kamk.fi>
2025-04-07 14:46:11 +0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("<H1 style=color:blue;>Express on Vercel is working! 2</H1>"));
+app.listen(4000, () => console.log("Server ready on port 4000."));
+
+module.exports = app;
+
